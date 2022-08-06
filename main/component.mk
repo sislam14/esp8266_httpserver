367c12b0a082d58502091c58597af86c00326368
shariful Islam <shariful1414.islam@gmail.com>
2022-08-06 14:42:09 +0600

    -Initial commit

---
+++ main/component.mk
@@ -0,0 +1,6 @@
+#
+# "main" pseudo-component makefile.
+#
+# (Uses default behaviour of compiling all source files in directory, adding 'include' to include path.)
+
+COMPONENT_EMBED_FILES :=  ${PROJECT_PATH}/webpage/favicon.ico ${PROJECT_PATH}/webpage/app.css ${PROJECT_PATH}/webpage/app.js ${PROJECT_PATH}/webpage/index.html ${PROJECT_PATH}/webpage/jquery-3.3.1.min.js
